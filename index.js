19ab6c16937143adee0b2f3f2560e0c7bf2d5923
midhunskr <midhun.shankar.199@gmail.com>
2024-09-19 11:39:28 +0530

    commit 3

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const app = express()
 
 //CORS for Authentication
 app.use(cors({
-  origin: ['https://chewse-food-delivery.vercel.app', 'http://localhost:5173'],
+  // origin: ['https://chewse-food-delivery.vercel.app', 'http://localhost:5173'],
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
